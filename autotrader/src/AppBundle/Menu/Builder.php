917e4695a48d9b4f1b509cc822331ba4749d5811
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-07-21 20:25:11 +0200

    download bundle by composer - error :(

---
+++ autotrader/src/AppBundle/Menu/Builder.php
@@ -0,0 +1,14 @@
+<?php
+
+use Knp\Menu\MenuFactory;
+
+class Builder
+{
+    public function mainMenu(MenuFactory $factory, array $options)
+    {
+        $menu = $factory->createItem('root');
+        $menu->addChild('Home', ['route' => 'homepage']);
+
+        return $menu;
+    }
+}
