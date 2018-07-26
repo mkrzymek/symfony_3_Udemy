e28d1913ff30d5d9774392fabd8d30104eb6c61a
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-07-26 14:41:02 +0200

    form fix

--- autotrader/src/CarBundle/Form/CarType.php
+++ autotrader/src/CarBundle/Form/CarType.php
@@ -4,6 +4,7 @@ namespace CarBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -30,6 +31,7 @@ class CarType extends AbstractType
                 ]
             ])
             ->add('navigation')
+            ->add('promote')
             ->add('description')
             ->add('model', EntityType::class, [
                 'required' => true,
