183e8c1e10725b349ae08e3129081a2826a5d432
Michal Krzymiński <michal.krzyminski@onwelo.com>
2018-07-26 13:20:30 +0200

    install and configure doctrine fixtures

--- autotrader/app/AppKernel.php
+++ autotrader/app/AppKernel.php
@@ -18,6 +18,7 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new CarBundle\CarBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
